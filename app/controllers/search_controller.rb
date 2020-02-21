ae83a094561fccb9cddc626fd2b1f72f801561e0
Alison Vermeil <alison.minton@gmail.com>
2020-02-21 10:19:49 -0700

    Symbolize names and remove extra comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,15 @@
 class SearchController < ApplicationController
   def index
     # http://openlibrary.org/search.json?title=the+man+who+saw+everything
-    # title = hash["docs"].first["title_suggest"]
-    # publisher = hash["docs"].first["Bloomsbury Publishing"]
-    # author = hash["docs"].first["author_name"]
     search_title = params[:title]
     conn = Faraday.new(:url => 'http://openlibrary.org/search.json?')
     response = conn.get do |req|
       req.params['title'] = search_title
     end
 
-    book_hash = JSON.parse(response.body)["docs"].first
-    book_title = book_hash["title"]
-    authors = book_hash["author_name"]
-    publisher = book_hash["publisher"]
-    genres = book_hash["subject"]
-    require "pry"; binding.pry
+    book_hash = JSON.parse(response.body, symbolize_names: true)[:docs].first
 
-    @book = Book.create(title: book_title, author: authors, publisher: publisher, genres: genres)
+    @book = Book.new(book_hash)
   end
 
 end
