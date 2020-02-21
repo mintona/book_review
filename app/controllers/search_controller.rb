cf21c7e64e5e1cc7d30330a300e78a33cee5a178
Alison Vermeil <alison.minton@gmail.com>
2020-02-21 11:18:41 -0700

    Add more to nyt service, tests still passing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,9 @@ class SearchController < ApplicationController
     book_info = LibraryService.new.get_book(params[:title])
     @book = Book.new(book_info)
 
-    nyt_response = NytService.new.get_reviews(params[:title])
+    reviews_info = NytService.new.get_reviews(params[:title])
 
-    nyt_hash = JSON.parse(nyt_response.body, symbolize_names: true)
-
-    reviews_hash = nyt_hash[:results]
-    @reviews = reviews_hash.map do |review_info|
+    @reviews = reviews_info.map do |review_info|
       Review.new(review_info)
     end
   end
--- app/services/nyt_service.rb
+++ app/services/nyt_service.rb
@@ -2,5 +2,6 @@ class NytService
 
   def get_reviews(book_title)
     nyt_response = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{book_title}&api-key=#{ENV['KEY']}")
+    nyt_hash = JSON.parse(nyt_response.body, symbolize_names: true)[:results]
   end
 end
