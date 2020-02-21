f252012fd495f13a4b0ad4a8f858f45a542974af
Alison Vermeil <alison.minton@gmail.com>
2020-02-21 11:00:00 -0700

    Fix problem with env key and add into controller with reviews connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,22 @@ class SearchController < ApplicationController
   def index
     # http://openlibrary.org/search.json?title=the+man+who+saw+everything
     search_title = params[:title]
-    conn = Faraday.new(:url => 'http://openlibrary.org/search.json?')
-    response = conn.get do |req|
+    library_conn = Faraday.new(:url => 'http://openlibrary.org/search.json?')
+    library_response = library_conn.get do |req|
       req.params['title'] = search_title
     end
 
-    book_hash = JSON.parse(response.body, symbolize_names: true)[:docs].first
+    book_hash = JSON.parse(library_response.body, symbolize_names: true)[:docs].first
 
     @book = Book.new(book_hash)
+
+    nyt_response = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{search_title}&api-key=#{ENV['KEY']}")
+    nyt_hash = JSON.parse(nyt_response.body, symbolize_names: true)
+
+    reviews_hash = nyt_hash[:results]
+    @reviews = reviews_hash.map do |review_info|
+      Review.new(review_info)
+    end
   end
 
 end
