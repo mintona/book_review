ad10067f46086be1a583cb2f8b2e68f85ea0b99b
Alison Vermeil <alison.minton@gmail.com>
2020-02-21 11:07:15 -0700

    Remove comments, tests are fulling passing at this point

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,15 @@
 class SearchController < ApplicationController
   def index
-    # http://openlibrary.org/search.json?title=the+man+who+saw+everything
-    search_title = params[:title]
     library_conn = Faraday.new(:url => 'http://openlibrary.org/search.json?')
     library_response = library_conn.get do |req|
-      req.params['title'] = search_title
+      req.params['title'] = params[:title]
     end
 
     book_hash = JSON.parse(library_response.body, symbolize_names: true)[:docs].first
 
     @book = Book.new(book_hash)
 
-    nyt_response = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{search_title}&api-key=#{ENV['KEY']}")
+    nyt_response = Faraday.get("https://api.nytimes.com/svc/books/v3/reviews.json?title=#{params[:title]}&api-key=#{ENV['KEY']}")
     nyt_hash = JSON.parse(nyt_response.body, symbolize_names: true)
 
     reviews_hash = nyt_hash[:results]
@@ -19,5 +17,4 @@ class SearchController < ApplicationController
       Review.new(review_info)
     end
   end
-
 end
